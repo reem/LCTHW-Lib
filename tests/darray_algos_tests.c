a0cca23c01ca0eac0d6b5e2adcbe7b14c5266dfd
Jonathan Reem <jonathan.reem@gmail.com>
2013-11-24 15:00:16 -0500

    Added speed tests.

--- tests/darray_algos_tests.c
+++ tests/darray_algos_tests.c
@@ -1,25 +1,36 @@
 #include "minunit.h"
 #include <lcthw/darray_algos.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 
-int testcmp(char **a, char **b) {
-    return strcmp(*a, *b);
+static inline int intcmp(int **a, int **b) {
+    return **a - **b;
 }
 
-DArray *create_words() {
-    DArray *result = DArray_create(0, 5);
-    char *words[] = {"asdfasfd", "werwar", "13234", "asdfasfd", "oioj"};
-    int i = 0;
-    for(i = 0; i < 5; i++) {
-        DArray_push(result, words[i]);
+static inline int sintcmp(int *a, int *b) {
+    return *a - *b;
+}
+
+int make_random(DArray *array, size_t n) {
+    srand(time(NULL));
+    size_t i = 0;
+    for(i = 0; i < n; i++) {
+        int *random = DArray_new(array);
+        *random = rand();
+        check(DArray_push(array, random) == 0, "Inserting random values failed.");
     }
 
-    return result;
+    return 0;
+
+error:
+    return -1;
 }
 
-int is_sorted(DArray *array) {
+int is_sorted(DArray *array, DArray_compare cmp) {
     int i = 0;
     for(i = 0; i < DArray_count(array) - 1; i++) {
-        if(strcmp(DArray_get(array, i), DArray_get(array, i+1)) > 0) {
+        if(cmp(DArray_get(array, i), DArray_get(array, i+1)) > 0) {
             return 0;
         }
     }
@@ -28,15 +39,19 @@ int is_sorted(DArray *array) {
 }
 
 char *run_sort_test(int (*func)(DArray *, DArray_compare), const char *name) {
-    DArray *words = create_words();
-    mu_assert(!is_sorted(words), "Words should start not sorted.");
+    DArray *nums = DArray_create(sizeof(int *), 5);
+    int rc = make_random(nums, 5);
+    mu_assert(rc == 0, "Randomization failed.");
+
+    mu_assert(!is_sorted(nums, (DArray_compare)sintcmp), "Numbers should start not sorted.");
 
     debug("--- Testing %s sorting algorithm", name);
-    int rc = func(words, (DArray_compare)testcmp);
+    rc = func(nums, (DArray_compare)intcmp);
     mu_assert(rc == 0, "Sort failed.");
-    mu_assert(is_sorted(words), "Sort didn't sort properly.");
 
-    DArray_destroy(words);
+    mu_assert(is_sorted(nums, (DArray_compare)sintcmp), "Sort didn't sort properly.");
+
+    DArray_clear_destroy(nums);
 
     return NULL;
 }
@@ -53,12 +68,83 @@ char *test_mergesort() {
     return run_sort_test(DArray_mergesort, "mergesort");
 }
 
+char *speed_sort_test(int (*func)(DArray *, DArray_compare), const char *name) {
+    size_t N = 10000;
+    debug("--- Testing the speed of %s", name);
+    DArray *source = DArray_create(sizeof(void *), N+1);
+
+    clock_t fastest = LONG_MAX;
+
+    int rc = make_random(source, N);
+    mu_assert(rc == 0, "Randomizing the source DArray failed.");
+
+    int i = 0;
+    for(i = 0; i < 25; i++) {
+        DArray *test = DArray_create(sizeof(int *), N+1);
+        rc = DArray_copy(source, test);
+        mu_assert(rc == 0, "Copy failed.");
+
+        clock_t elapsed = -clock();
+        rc = func(test, (DArray_compare)intcmp);
+        elapsed += clock();
+        mu_assert(rc == 0, "Sort failed.");
+
+        mu_assert(is_sorted(test, (DArray_compare)sintcmp), "Sort didn't sort properly.");
+
+        if(elapsed < fastest) fastest = elapsed;
+        DArray_destroy(test);
+    }
+
+    debug("Fastest time for sort: %s, size %zu: %f", name, N, ((float)fastest)/CLOCKS_PER_SEC);
+
+    DArray_clear_destroy(source);
+    return NULL;
+}
+
+char *test_speed_qsort() {
+    return speed_sort_test(DArray_qsort, "quicksort");
+}
+
+char *test_speed_mergesort() {
+    return speed_sort_test(DArray_mergesort, "mergesort");
+}
+
+char *test_speed_heapsort() {
+    return speed_sort_test(DArray_heapsort, "heapsort");
+}
+
+char *test_cmp() {
+    DArray *fake = DArray_create(sizeof(int), 10);
+    int *num1 = DArray_new(fake);
+    int *num2 = DArray_new(fake);
+
+    *num1 = 100;
+    *num2 = 20;
+    mu_assert(sintcmp(num1, num2) > 0, "Comparison fails on 100, 20.");
+
+    *num1 = 50;
+    *num2 = 50;
+    mu_assert(sintcmp(num1, num2) == 0, "Comparison fails on 50, 50.");
+
+    *num1 = 30;
+    *num2 = 60;
+    mu_assert(sintcmp(num1, num2) < 0, "Comparison fails on 30, 60.");
+
+    DArray_clear_destroy(fake);
+
+    return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
 
+    mu_run_test(test_cmp);
     mu_run_test(test_qsort);
     mu_run_test(test_heapsort);
     mu_run_test(test_mergesort);
+    mu_run_test(test_speed_qsort);
+    mu_run_test(test_speed_mergesort);
+    mu_run_test(test_speed_heapsort);
 
     return NULL;
 }
